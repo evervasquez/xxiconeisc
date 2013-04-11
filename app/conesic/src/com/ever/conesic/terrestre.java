d64dfbf96974c507484c897ee816b325dd441989
medalol <mauro_1142@hotmail.es>
2013-04-11 18:35:01 -0500

    mapa vale

--- app/conesic/src/com/ever/conesic/terrestre.java
+++ app/conesic/src/com/ever/conesic/terrestre.java
@@ -71,7 +71,7 @@ public class terrestre extends SherlockFragment {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map)).getMap();
+            mMap = ((SupportMapFragment) getSherlockActivity().getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
